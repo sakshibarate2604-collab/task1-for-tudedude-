3aaef6d88775b5c4bfc10ae3988b5acaf5f45c5b
sakshibarate2604-collab <sakshibarate2604@gmail.com>
2026-01-15 11:40:11 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,11 @@
+# Task 2: Personalized Greeting
+
+# Take user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate full name
+full_name = first_name + " " + last_name
+
+# Print greeting
+print("Hello,", full_name + "! Welcome to Python programming")
